1956025dcafa73499cf3022921fa75c0aab36268
Nathan Bianco <nathan.bianco@tamu.edu>
2020-09-04 16:00:31 -0500

    MP1 Add test cases w/o Calculator; improve print format; improve code to remove warnings

--- src/com/group3/MP1/Main_MP1.java
+++ src/com/group3/MP1/Main_MP1.java
@@ -6,13 +6,14 @@ public class Main_MP1 {
 
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
-        Calculator mycalc = new Calculator();
-        mycalc.setname("Group 3");
+        // Calculator mycalc = new Calculator();
+        // mycalc.setname("Group 3");
+        String mycalc = "Group 3"; // TODO: delete this
 
         while (true) {
             // Welcome user to Calculator and take user input
-            System.out.println("\nWelcome to the Calculator designed by " + mycalc.getname() + ".\n"
-                + "Enter 'A' to Add, 'S' to Subtract, 'M' to Multiply, or 'Q' to Quit.\n");
+            System.out.println("\nWelcome to the Calculator designed by " + mycalc /*TODO: change this*/ + ".\n"
+                + "Enter 'A' to Add, 'S' to Subtract, 'M' to Multiply, or 'Q' to Quit.");
             String user_choice = input.nextLine();
 
             // if user chooses to quit
@@ -28,47 +29,46 @@ public class Main_MP1 {
             }
 
             // Take argument inputs, making the user retry for invalid inputs
-            String arg1_input = "";
-            String arg2_input = "";
-            Float arg1 = 0f;
-            Float arg2 = 0f;
+            String arg1_input;
+            String arg2_input;
+            float arg1;
+            float arg2;
             while (true) {
-                System.out.println("Enter argument 1: ");
+                System.out.print("Enter argument 1: ");
                 arg1_input = input.nextLine();
                 try {
-                    arg1 = Float.parseFloat(arg1_input + 'f');
+                    arg1 = Float.parseFloat(arg1_input);
                     break;
                 }
-                catch (Exception e) {
-                    continue;
-                }
+                catch (Exception e) { /* do nothing */ }
             }
             while (true) {
-                System.out.println("Enter argument 2: ");
+                System.out.print("Enter argument 2: ");
                 arg2_input = input.nextLine();
                 try {
-                    arg2 = Float.parseFloat(arg2_input + 'f');
+                    arg2 = Float.parseFloat(arg2_input);
                     break;
                 }
-                catch (Exception e) {
-                    continue;
-                }
+                catch (Exception e) { /* do nothing */ }
             }
 
             // Perform calculation and print result
-            String result_type = "";
-            Float ans = 0f;
+            String result_type;
+            float ans;
             if (user_choice.contains("A")) {
                 result_type = "sum";
-                ans = mycalc.addition(arg1, arg2);
+                // ans = mycalc.addition(arg1, arg2);
+                ans = arg1 + arg2; // TODO: delete this
             }
             else if (user_choice.contains("S")) {
                 result_type = "difference";
-                ans = mycalc.subtraction(arg1, arg2);
+                // ans = mycalc.subtraction(arg1, arg2);
+                ans = arg1 - arg2; // TODO: delete this
             }
             else {
                 result_type = "product";
-                ans = mycalc.multiplication(arg1, arg2);
+                // ans = mycalc.multiplication(arg1, arg2);
+                ans = arg1 * arg2; // TODO: delete this
             }
             System.out.println("\nThe " + result_type + " of " + arg1 + " and " + arg2 + " is " + ans);
         }
