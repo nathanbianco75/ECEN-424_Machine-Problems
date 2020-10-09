3fae368d021a4240f9b6228421a71d204ab088e1
Nathan Bianco <nathan.bianco@tamu.edu>
2020-10-09 17:48:28 -0500

    MP3: Set up file system

---
+++ src/com/group3/MP3/Client/Main_MP3_Client.java
@@ -0,0 +1,11 @@
+package com.group3.MP3.Client;
+
+import com.group3.MP2.Matrix;
+import com.group3.MP2.MatrixCalculator;
+
+public class Main_MP3_Client {
+
+    public static void main(String[] args) {
+        ;
+    }
+}
---
+++ src/com/group3/MP3/Server/Main_MP3_Server.java
@@ -0,0 +1,11 @@
+package com.group3.MP3.Server;
+
+import com.group3.MP2.Matrix;
+import com.group3.MP2.MatrixCalculator;
+
+public class Main_MP3_Server {
+
+    public static void main(String[] args) {
+        ;
+    }
+}
